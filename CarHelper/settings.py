272cefa1fa87d1453f868ad30e5a82a48c402680
CarlosAbielG <147435809+CarlosAbielG@users.noreply.github.com>
2026-05-14 03:12:53 +0000

    Merge branches

--- CarHelper/settings.py
+++ CarHelper/settings.py
@@ -25,9 +25,17 @@ SECRET_KEY = 'django-insecure-5)7_qqjcrrc=6aby4fq*erf9fg(pz(%)tm+#o%a%4+@90=o$%o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://localhost:8000",
+    "https://127.0.0.1:8000",
+]
 # Application definition
 
 INSTALLED_APPS = [
